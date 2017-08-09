38d04d059f17ae6a08a7b9c18f14dc786bd93761
ivsemionov <ivsemionov@iggn.permkrai.ru>
2017-08-09 09:25:14 +0500

    увеличил время ожидания загрузки

--- employer.py
+++ employer.py
@@ -19,29 +19,29 @@ profile.set_preference("geo.prompt.testing.allow", True)
 driver = webdriver.Firefox(profile)
 # заходим на страницу создания заявления (ссылка "Получить услугу")
 driver.get('https://59.gosuslugi.ru/service/5935949/5900000000161275583')
-time.sleep(6)  # секунды
+time.sleep(12)  # секунды
 # заполняем телефон
 tel = driver.find_element_by_class_name("flt_lbl_inp")
 tel.click()
-time.sleep(1)  # секунды
+time.sleep(3)  # секунды
 tel.send_keys(namespace.login)
 # пароль
 pas = driver.find_element_by_id("password")
 pas.click()
-time.sleep(1)  # секунды
+time.sleep(3)  # секунды
 pas.send_keys(namespace.password)
 # нажимаем кнопку войти
 but = driver.find_element_by_class_name("button-big")
 but.click()
-time.sleep(4)  # секунды
+time.sleep(10)  # секунды
 org = driver.find_element_by_id("org0")
 assert org.get_attribute("onclick") == 'orgPage.selectOrg(0);'
 org.click()
-time.sleep(10)  # секунды
+time.sleep(20)  # секунды
 # нажимаем кнопку новый черновик заявления
 but = driver.find_element_by_class_name("draft-new-button")
 but.click()
-time.sleep(20)  # секунды
+time.sleep(40)  # секунды
 # Данные документа, подтверждающего факт внесения сведений о юридическом лице в ЕГРЮЛ
 f = driver.find_element_by_xpath("//div[@id='Form5935949.FormStep1.UL.sved_ul']/div/textarea")
 f.send_keys("123")
@@ -70,10 +70,11 @@ f.send_keys("111")
 # email
 f = driver.find_element_by_xpath("//div[@id='Form5935949.FormStep1.Panel_ContactUL.email_ul']/div[1]/input")
 f.send_keys("ivsemionov@iggn.permkrai.ru")
+time.sleep(4)  # секунды
 # next
 f = driver.find_element_by_id("__nextStep")
 f.click()
-time.sleep(10)  # секунды
+time.sleep(20)  # секунды
 # этап 2
 # Номер квалификационного аттестата
 f = driver.find_element_by_xpath("//div[@id='Form5935949.FormStep2.Panel_Kval.num_kval']/div[1]/input")
@@ -103,32 +104,33 @@ f.send_keys("111")
 # Дата выдачи
 f = driver.find_element_by_xpath("//div[@id='Form5935949.FormStep2.Panel_Duty.duty_date']/div[1]/input")
 f.send_keys("12.12.2012")
+time.sleep(4)  # секунды
 # next
 f = driver.find_element_by_id("__nextStep")
 f.click()
-time.sleep(10)  # секунды
+time.sleep(20)  # секунды
 # этап 3
 # Копии учредительных документов
 f = driver.find_element_by_xpath("//div[@id='Form5935949.FormStep3.Panel_Doc.doc1']/div[3]/input")
 f.send_keys(os.path.dirname(os.path.realpath(__file__))+"\\1.pdf")
-time.sleep(2)  # секунды
+time.sleep(4)  # секунды
 # Опись прилагаемых документов
 f = driver.find_element_by_xpath("//div[@id='Form5935949.FormStep3.Panel_Doc.doc2']/div[3]/input")
 f.send_keys(os.path.dirname(os.path.realpath(__file__))+"\\1.pdf")
-time.sleep(2)  # секунды
+time.sleep(4)  # секунды
 # Копия квалификационного аттестата
 f = driver.find_element_by_xpath("//div[@id='Form5935949.FormStep3.Panel_Doc.doc3']/div[3]/input")
 f.send_keys(os.path.dirname(os.path.realpath(__file__))+"\\1.pdf")
-time.sleep(2)  # секунды
+time.sleep(4)  # секунды
 # Копия приказа о назначении должностного лица
 f = driver.find_element_by_xpath("//div[@id='Form5935949.FormStep3.Panel_Doc.AppointingOrder']/div[3]/input")
 f.send_keys(os.path.dirname(os.path.realpath(__file__))+"\\1.pdf")
-time.sleep(2)  # секунды
+time.sleep(4)  # секунды
 # next
 f = driver.find_element_by_id("__nextStep")
 f.click()
-time.sleep(10)  # секунды
+time.sleep(20)  # секунды
 # next
 f = driver.find_element_by_id("__nextStep")
 f.click()
-time.sleep(10)  # секунды
+time.sleep(20)  # секунды
